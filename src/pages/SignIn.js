9467b237c0388678ca267c9789b312e06ef90dec
Shawn Jamil <shawnjamil313@gmail.com>
2022-11-11 11:59:12 +0600

    sign in , sign up and forgot password pages and OAuth components

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
-const signIn = () => {
-  return <div>signIn</div>;
+const SignIn = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [showPassword, setShowPassword] = useState(false);
+  const { email, password } = formData;
+
+  const onChange = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [event.target.id]: event.target.value,
+    }));
+  };
+  return (
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img
+            src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=773&q=80"
+            alt="key"
+            className="w-full rounded-2xl"
+          />
+        </div>
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form>
+            <div className="relative mb-6">
+              <input
+                className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+                type="email"
+                placeholder="Email Address"
+                value={email}
+                id="email"
+                onChange={onChange}
+              />
+              <input
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                value={password}
+                id="password"
+                onChange={onChange}
+              />
+
+              {showPassword ? (
+                <AiFillEyeInvisible
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                  className="absolute right-3 bottom-3 text-xl cursor-pointer"
+                />
+              ) : (
+                <AiFillEye
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                  className="absolute right-3 bottom-3 text-xl cursor-pointer"
+                />
+              )}
+            </div>
+            <div className="flex justify-between">
+              <p className="mb-6">
+                Don't have an account ?{" "}
+                <Link
+                  className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
+                  to="/sign-up"
+                >
+                  Register
+                </Link>
+              </p>
+              <p>
+                <Link
+                  className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out "
+                  to="/forgot-password"
+                >
+                  Forgot Password?
+                </Link>
+              </p>
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white px-7 py-3 rounded shadow-md uppercase font-medium hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+            >
+              Sign In
+            </button>
+            <div className="flex items-center my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
+              <p className="text-center mx-4 font-semibold">OR</p>
+            </div>
+            <OAuth />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
-export default signIn;
+export default SignIn;
